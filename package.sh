2622d61a85cb5d8bf487ed6c5efefca3bd7cda72
saihgupr <121105543+saihgupr@users.noreply.github.com>
2026-08-01 14:10:22 -0400

    chore: update VERSION to 1.4.2 in package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BUILD_DIR="${DIR}/build"
 DMG_MAKER_DIR="${DIR}/../DMGMaker"
-VERSION="1.4.1"
+VERSION="1.4.2"
 OUTPUT_NAME="NotifiCLI"
 
 echo "🔨 Starting clean build for v${VERSION}..."
